c84b3df974d6c26fe696730899572bfe075ea60c
MayHyeyeonKim <hkim7963@gmail.com>
2023-03-05 18:02:40 -0800

    add week8 admin.py

--- django/week7/week7app/admin.py
+++ django/week7/week7app/admin.py
@@ -1,3 +1,9 @@
+# admin.py
 from django.contrib import admin
+from week7app.models import Company
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'type')
+    list_display = ('name', 'address', 'type', 'employee')
+    list_filter = ('type', 'employee')
